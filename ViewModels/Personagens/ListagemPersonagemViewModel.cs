8a61ce7d4ebd3f6a5cc80231143e9c130bdb8ed8
Julia Reis <julyreis.correa@gmail.com>
2025-08-26 22:34:19 -0300

    PAM-II AppRpgEtec

---
+++ ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Services.Personagens;
+using AppRpgEtec.Models.Personagens;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+
+        public ObservableCollection<Personagem> Personagems { get; set; }
+
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagems = new ObservableCollection<Personagem>();
+        }
+
+        public async Task ObterPersonagens()
+        {
+            try
+            {
+                Personagems = await pService.GetPersonagemsAsync();
+                OnPropertyChanged(nameof(Personagems));
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+
+        }
+    }   
+}
+
